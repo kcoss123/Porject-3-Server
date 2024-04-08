eee755291ab42bbeeeb75ded52b3190baaa0923c
kcoss123 <159080978+kcoss123@users.noreply.github.com>
2024-04-08 02:10:43 -0400

    antoher attempt

--- routes/workorders.js
+++ routes/workorders.js
@@ -1,11 +1,14 @@
-const user = require("../models/user");
-const company = require("../models/company");
-const { WorkOrder } = require("../models/workorder");
-const { Thread } = require("../models/thread");
-const { Message } = require("../models/message");
 const express = require("express");
 const multer = require("multer");
+const path = require("path"); // Import the 'path' module
+
 const router = express.Router();
+
+const User = require("../models/user"); 
+const Company = require("../models/company"); 
+const WorkOrder = require("../models/workorder");
+const Thread = require("../models/thread");
+const Message = require("../models/message");
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -17,91 +20,96 @@ const storage = multer.diskStorage({
             file.fieldname + "-" + Date.now() + path.extname(file.originalname)
         );
     },
-    });
+});
+
 const upload = multer({ storage: storage });
 
 // list all workorders for logged in user
 router.post("/workorders", async (req, res) => {
-    const user = await user.findOne({ username: req.body.username });
-    const orders = await WorkOrder.find({ user: user.id });
-    if (orders.length) {
+    try {
+        const user = await User.findOne({ username: req.body.username });
+        if (!user) throw new Error("User not found");
+
+        const orders = await WorkOrder.find({ user: user.id });
         res.status(200).send(orders);
-    } else {
-        res.status(200).send();
+    } catch (error) {
+        res.status(400).send(error.message);
     }
-})
+});
 
 // add new workorder
 router.post("/addworkorder", upload.array("files", 5), async (req, res) => {
-    const user = await user.findOne({ username: req.body.username });
-    const company = await Company.findOne({ companyName: req.body.companyName });
-    var media;
-    // console.log(req.body.files);
-    if (req.body.files) {   // FIXME
-        media = req.body.files.map((file) => {
-            return {data: Buffer.from(file.buffer, "base64"), contentType: file.mimetype};
-        })
+    try {
+        const user = await User.findOne({ username: req.body.username });
+        if (!user) throw new Error("User not found");
+
+        const company = await Company.findOne({ companyName: req.body.companyName });
+        if (!company) throw new Error("Company not found");
+
+        let media;
+        if (req.body.files) {
+            media = req.body.files.map((file) => {
+                return { data: Buffer.from(file.buffer, "base64"), contentType: file.mimetype };
+            });
+        }
+
+        let order = new WorkOrder({
+            title: req.body.title,
+            description: req.body.desc,
+            userMedia: media,
+            user: user.id,
+            company: company.id,
+            companyName: req.body.companyName,
+        });
+
+        let thread = new Thread({
+            title: req.body.title,
+            user: user.id,
+            company: company.id,
+            workorder: order.id,
+            workorderName: order.title,
+        });
+
+        await order.save();
+        await thread.save();
+
+        res.status(200).send(thread);
+    } catch (error) {
+        res.status(400).send(error.message);
     }
-
-    let order = new WorkOrder({
-        title: req.body.title,
-        description: req.body.desc,
-        userMedia: media,
-        user: user.id,
-        company: company.id,
-        companyName: req.body.companyName,
-    });
-
-    let thread = new Thread({
-        title: req.body.title,
-        user: user.id,
-        company: company.id,
-        workorder: order.id,
-        workorderName: order.title,
-    });
-
-    await order.save().then(order => {
-    }).catch(error => {
-        res.status(400).send(error);
-    })
-    await thread.save().then(order => {
-        res.status(200).send(order);
-    }).catch(error => {
-        res.status(400).send(error);
-    })
-})
+});
 
 // retrieve workorder messages
 router.post("/workordermessages", async (req, res) => {
-    const thread = await Thread.findOne({ workorder: req.body.orderId });
-    const messages = await Message.find({ thread: thread.id }).sort({ timestamp: 1 });
-    const data = {
-        messages: messages,
-        workorderName: thread.workorderName,
-    }
     try {
+        const thread = await Thread.findOne({ workorder: req.body.orderId });
+        const messages = await Message.find({ thread: thread.id }).sort({ timestamp: 1 });
+        const data = {
+            messages: messages,
+            workorderName: thread.workorderName,
+        };
         res.status(200).send(data);
     } catch (error) {
-        res.status(400).send(error);
+        res.status(400).send(error.message);
     }
-})
+});
 
 router.post("/addworkordermessage", async (req, res) => {
-    const thread = await Thread.findOne({ workorder: req.body.orderId });
-    let msg = new Message({
-        content: {
-            text: req.body.newMsg,
-        },
-        thread: thread.id,
-        sender: req.body.username,
-    })
-    await msg.save().then(msg => {
+    try {
+        const thread = await Thread.findOne({ workorder: req.body.orderId });
+        let msg = new Message({
+            content: {
+                text: req.body.newMsg,
+            },
+            thread: thread.id,
+            sender: req.body.username,
+        });
+        await msg.save();
         res.status(200).send(msg);
-    }).catch(error => {
-        res.status(400).send(error);
-    })
-    return;
-})
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+});
 
 router.post("/deleteworkorder", async (req, res) => {
     try {
@@ -111,8 +119,8 @@ router.post("/deleteworkorder", async (req, res) => {
         await WorkOrder.findByIdAndDelete(req.body.orderId);
         res.status(200).send();
     } catch (error) {
-        res.status(400).send(error);
+        res.status(400).send(error.message);
     }
-})
+});
 
 module.exports = router;
